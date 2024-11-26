85ec809700db3e6b3ed00af808ff8dfb36f1816c
Akhilesh Jadhav <akhileshjadhav26@gmail.com>
2024-11-26 15:51:55 -0500

    chore: Configure ESLint and static export

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/Akhilesh-portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Akhilesh-portfolio/' : '',
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
