6966527a89b1ed02d7f04f3adb2d6d8a426f5385
Akhilesh Jadhav <57318115+asJ26@users.noreply.github.com>
2024-12-03 11:05:59 -0500

    Update projects.ts

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,24 +1,25 @@
 export const projects = [
   {
-    title: "Check-out this Portfolio Website on my GitHub",
-    description: "Modern portfolio built with Next.js 14, Tailwind CSS, and Framer Motion. Features smooth animations and responsive design.",
-    image: "/images/projects/Porfolio.png",
-    githubLink: "https://github.com/asJ26/Akhilesh-portfolio.git",
-    skills: ["Next.js", "TypeScript", "Tailwind CSS", "Framer Motion"]
-  },
-   {
-    title: "Netflix Streaming Analytics Platform",
+    title: "Streaming Analytics Platform",
     description: "Architected a robust GCP data pipeline processing over 100,000 viewing events with a 40% reduction in processing time, and designed advanced Google Data Studio dashboards that boosted strategic decision-making efficiency by 25%.",
     image: "/images/projects/Netflix.png",
     githubLink: "https://github.com/asJ26/Netflix-Data-Pipeline-and-Dashboard.git",
     skills: ["BigQuery","Google Cloud Platform (GCP)","Dataflow","Looker Studio","Python"]
   },
+ 
   {
     title: "IoT Sensor Data Pipeline",
     description: "Establishes a real-time data analytics pipeline using AWS, simulating IoT sensor data generation, processing it through AWS Lambda, storing in Amazon S3, and visualizing via Amazon QuickSight for dynamic insights.",
     image: "/images/projects/IOT_pipeline.png",
     githubLink: "https://github.com/asJ26/IoT-sensor-Data-Pipeline.git",
     skills: ["AWS Kinesis", "AWS Lambda", "S3", "QuickSight", "CLI"]
+  },
+   {
+    title: "Sentiment Analytics Pipeline",
+    description: "Developed a real-time railway tweet analysis platform using Python, Django, and Azure services to process social media data for sentiment analysis and emergency detection, enhancing railway operations.",
+    image: "/images/projects/twitter.png",
+    githubLink: "https://github.com/asJ26/Twitter-pipeline-AZURE.git",
+    skills: ["Azure", "Django", "PostgreSQL", "Python"]
   },
   {
     title: "Syracuse DBMS",
