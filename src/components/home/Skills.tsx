92268919145e6ee6c4a60bb686f10e3d1c660dae
Akhilesh Jadhav <57318115+asJ26@users.noreply.github.com>
2025-03-01 17:48:58 -0500

    Update Skills.tsx

--- src/components/home/Skills.tsx
+++ src/components/home/Skills.tsx
@@ -4,79 +4,73 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { FloatingSkill } from '../ui/floating-skill';
 import { 
-  SiPython, SiScala, SiJavascript, SiCplusplus, SiR,
-  SiApachespark, SiApachekafka, SiApachehadoop,
-  SiApacheflink, SiApachecassandra, SiApacheairflow,
-  SiAmazon, SiAmazons3, SiMicrosoftazure,
-  SiGooglecloud, SiMysql, SiPostgresql,
-  SiMicrosoftsqlserver, SiOracle, SiSnowflake,
-  SiMongodb, SiRedis, SiDocker, SiKubernetes,
-  SiJenkins, SiGit, SiGrafana,
-  SiAnsible, SiTensorflow,
-  SiPytorch, SiScikitlearn,
-  SiFlask, SiDjango, SiExpress, SiSpringboot,
-  SiPowerbi, SiTableau, SiDbt
+  SiPython, SiJavascript, SiTypescript, SiGolang,
+  SiHtml5, SiCss3, SiTailwindcss, SiJquery, SiAjax, 
+  SiRedux, SiGraphql, SiBootstrap, SiApachekafka,
+  SiAmazonaws, SiAmazons3, SiAmazonec2, SiDynamodb,
+  SiMicrosoftazure, SiGooglecloud, SiMysql, SiPostgresql,
+  SiMicrosoftsqlserver, SiOracle, SiMongodb, SiRedis,
+  SiDocker, SiKubernetes, SiJenkins, SiGit, SiGithub,
+  SiGitlab, SiSpring, SiReact, SiNextdotjs, SiNodedotjs,
+  SiAngular, SiVisualstudiocode, SiIntellijidea,
+  SiEclipseide, SiSalesforce, SiPostman, SiJira,
+  SiConfluence
 } from 'react-icons/si';
 import { 
-  FaJava, FaTerminal, FaServer
+  FaJava, FaTerminal, 
+  FaCplusplus, FaSwift, 
 } from 'react-icons/fa';
-import { DiNginx } from 'react-icons/di';
+import { TbBrandCSharp } from 'react-icons/tb';
 
 // Organize skills into rows for pyramid layout
 const skillRows = [
-  // First row - 12 skills
+  // First row - Programming Languages
   [
-    { name: 'Python', icon: SiPython, color: '#3776AB' },
     { name: 'Java', icon: FaJava, color: '#007396' },
+    { name: 'Python', icon: SiPython, color: '#3776AB' },
     { name: 'JavaScript', icon: SiJavascript, color: '#F7DF1E' },
-    { name: 'Scala', icon: SiScala, color: '#DC322F' },
-    { name: 'AWS', icon: SiAmazon, color: '#FF9900' },
+    { name: 'TypeScript', icon: SiTypescript, color: '#3178C6' },
+    { name: 'C++', icon: FaCplusplus, color: '#00599C' },
+    { name: 'C', icon: TbBrandCSharp, color: '#A8B9CC' },
+    { name: 'Swift', icon: FaSwift, color: '#FA7343' },
+    { name: 'Golang', icon: SiGolang, color: '#00ADD8' },
+    { name: 'Shell', icon: FaTerminal, color: '#4EAA25' },
+  ],
+  // Second row - Web & Cloud
+  [
+    { name: 'HTML5', icon: SiHtml5, color: '#E34F26' },
+    { name: 'CSS3', icon: SiCss3, color: '#1572B6' },
+    { name: 'Tailwind', icon: SiTailwindcss, color: '#38B2AC' },
+    { name: 'jQuery', icon: SiJquery, color: '#0769AD' },
+    { name: 'Redux', icon: SiRedux, color: '#764ABC' },
+    { name: 'GraphQL', icon: SiGraphql, color: '#E10098' },
+    { name: 'AWS', icon: SiAmazonaws, color: '#FF9900' },
     { name: 'Azure', icon: SiMicrosoftazure, color: '#0089D6' },
     { name: 'GCP', icon: SiGooglecloud, color: '#4285F4' },
+  ],
+  // Third row - Frameworks & DevOps
+  [
+    { name: 'Spring', icon: SiSpring, color: '#6DB33F' },
+    { name: 'React.js', icon: SiReact, color: '#61DAFB' },
+    { name: 'Next.js', icon: SiNextdotjs, color: '#000000' },
+    { name: 'Node.js', icon: SiNodedotjs, color: '#339933' },
+    { name: 'Angular', icon: SiAngular, color: '#DD0031' },
     { name: 'Docker', icon: SiDocker, color: '#2496ED' },
     { name: 'Kubernetes', icon: SiKubernetes, color: '#326CE5' },
-    { name: 'TensorFlow', icon: SiTensorflow, color: '#FF6F00' },
-    { name: 'PyTorch', icon: SiPytorch, color: '#EE4C2C' },
-    { name: 'scikit-learn', icon: SiScikitlearn, color: '#F7931E' },
-  ],
-  // Second row - 11 skills
-  [
-    { name: 'Spark', icon: SiApachespark, color: '#E25A1C' },
-    { name: 'Kafka', icon: SiApachekafka, color: '#D22128' },
-    { name: 'Hadoop', icon: SiApachehadoop, color: '#FF652F' },
-    { name: 'Flink', icon: SiApacheflink, color: '#E6526F' },
-    { name: 'MySQL', icon: SiMysql, color: '#4479A1' },
-    { name: 'PostgreSQL', icon: SiPostgresql, color: '#336791' },
-    { name: 'MongoDB', icon: SiMongodb, color: '#47A248' },
-    { name: 'Redis', icon: SiRedis, color: '#DC382D' },
-    { name: 'Flask', icon: SiFlask, color: '#3775A9' },
-    { name: 'Django', icon: SiDjango, color: '#092E20' },
-    { name: 'Express', icon: SiExpress, color: '#68A063' },
-  ],
-  // Third row - 10 skills
-  [
-    { name: 'C++', icon: SiCplusplus, color: '#00599C' },
-    { name: 'R', icon: SiR, color: '#276DC3' },
-    { name: 'Shell', icon: FaTerminal, color: '#4EAA25' },
     { name: 'Jenkins', icon: SiJenkins, color: '#D24939' },
     { name: 'Git', icon: SiGit, color: '#F05032' },
-    { name: 'Grafana', icon: SiGrafana, color: '#F46800' },
-    { name: 'Ansible', icon: SiAnsible, color: '#EE0000' },
-    { name: 'Nginx', icon: DiNginx, color: '#009639' },
-    { name: 'Spring', icon: SiSpringboot, color: '#6DB33F' },
-    { name: 'Airflow', icon: SiApacheairflow, color: '#017CEE' },
   ],
-  // Fourth row - 9 skills
+  // Fourth row - Databases & Tools
   [
-    { name: 'NiFi', icon: FaServer, color: '#728E9B' },
-    { name: 'Cassandra', icon: SiApachecassandra, color: '#1287B1' },
-    { name: 'S3', icon: SiAmazons3, color: '#569A31' },
-    { name: 'SQL Server', icon: SiMicrosoftsqlserver, color: '#CC2927' },
+    { name: 'MongoDB', icon: SiMongodb, color: '#47A248' },
+    { name: 'MySQL', icon: SiMysql, color: '#4479A1' },
+    { name: 'PostgreSQL', icon: SiPostgresql, color: '#336791' },
     { name: 'Oracle', icon: SiOracle, color: '#F80000' },
-    { name: 'Snowflake', icon: SiSnowflake, color: '#29B5E8' },
-    { name: 'Power BI', icon: SiPowerbi, color: '#F2C811' },
-    { name: 'Tableau', icon: SiTableau, color: '#E97627' },
-    { name: 'dbt', icon: SiDbt, color: '#FF694B' },
+    { name: 'SQL Server', icon: SiMicrosoftsqlserver, color: '#CC2927' },
+    { name: 'Redis', icon: SiRedis, color: '#DC382D' },
+    { name: 'Kafka', icon: SiApachekafka, color: '#231F20' },
+    { name: 'JIRA', icon: SiJira, color: '#0052CC' },
+    { name: 'Postman', icon: SiPostman, color: '#FF6C37' },
   ],
 ];
 
